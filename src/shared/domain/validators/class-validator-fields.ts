e46513131346c25c9c4183c4214d6f1587ebef2d
GuilhermeMooser <guilhermemoserm@gmail.com>
2025-04-21 18:01:59 -0300

    Abstract class ClassValidatorFields

---
+++ src/shared/domain/validators/class-validator-fields.ts
@@ -0,0 +1,30 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+import { validateSync } from 'class-validator';
+import {
+  FieldsErrors,
+  ValidatorFieldsInterface,
+} from './validator-fields.interface';
+
+export abstract class ClassValidatorFields<PropsValidated>
+  implements ValidatorFieldsInterface<PropsValidated>
+{
+  errors: FieldsErrors;
+  validatedData: PropsValidated;
+
+  validate(data: any): boolean {
+    const errors = validateSync(data);
+    if (errors.length) {
+      this.errors = {};
+      for (const error of errors) {
+        const field = error.property;
+        this.errors[field] = Object.values(error.constraints);
+      }
+    } else {
+      this.validatedData = data;
+    }
+    return !errors.length;
+  }
+}
