578b7531b23dcc8dfbaa9f55423bfff9fd1039e9
GuilhermeMooser <guilhermemoserm@gmail.com>
2025-04-30 21:36:09 -0300

    InMemoryRepository unit tests - findAll method

--- src/shared/domain/repositories/__tests__/unit/in-memory.repository.spec.ts
+++ src/shared/domain/repositories/__tests__/unit/in-memory.repository.spec.ts
@@ -38,4 +38,12 @@ describe('InMemoryRepository unit tests', () => {
     const result = await sut.findById(entity.id);
     expect(entity.toJSON()).toStrictEqual(result.toJSON());
   });
+
+  it('Should return all entities', async () => {
+    const entity = new StubEntity({ name: 'Test Name', price: 50 });
+    await sut.insert(entity);
+
+    const result = await sut.findAll();
+    expect([entity]).toStrictEqual(result);
+  });
 });
