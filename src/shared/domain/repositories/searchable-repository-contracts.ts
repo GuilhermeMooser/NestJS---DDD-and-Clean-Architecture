8ca6fde84e9cfdb24d5faf7e44061f370c6aca30
GuilhermeMooser <guilhermemoserm@gmail.com>
2025-05-11 19:35:12 -0300

    Unit tests SearchParams class - 1

--- src/shared/domain/repositories/searchable-repository-contracts.ts
+++ src/shared/domain/repositories/searchable-repository-contracts.ts
@@ -19,12 +19,12 @@ export class SearchParams {
   protected _sortDir: SortDirection | null;
   protected _filter: string | null;
 
-  constructor(props: SearchProps) {
-    this._page = props.page;
-    this._perPage = props.perPage;
-    this._sort = props.sort;
-    this._sortDir = props.sortDir;
-    this._filter = props.filter;
+  constructor(props: SearchProps = {}) {
+    this.page = props.page;
+    this.perPage = props.perPage;
+    this.sort = props.sort;
+    this.sortDir = props.sortDir;
+    this.filter = props.filter;
   }
 
   get page() {
@@ -44,7 +44,7 @@ export class SearchParams {
   }
 
   private set perPage(value: number) {
-    let _perPage = +value;
+    let _perPage = value === (true as any) ? this._perPage : +value;
     if (
       Number.isNaN(_perPage) ||
       _perPage <= 0 ||
@@ -82,7 +82,10 @@ export class SearchParams {
     return this._filter;
   }
 
-  private set filter(value: string | null) {}
+  private set filter(value: string | null) {
+    this._filter =
+      value === null || value === undefined || value === '' ? null : `${value}`;
+  }
 }
 
 export interface SearchableRepositoryInterface<
