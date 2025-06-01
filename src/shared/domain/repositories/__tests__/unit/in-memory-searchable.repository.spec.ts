c55edaf7c51f0efd09ac87ca2a55ee64a3b56634
GuilhermeMooser <guilhermemoserm@gmail.com>
2025-06-01 17:32:22 -0300

    Unit tests InMemorySearchableRepository class - applyFilter

--- src/shared/domain/repositories/__tests__/unit/in-memory-searchable.repository.spec.ts
+++ src/shared/domain/repositories/__tests__/unit/in-memory-searchable.repository.spec.ts
@@ -33,10 +33,41 @@ describe('InMemorySearchableRepository unit tests', () => {
     sut = new StubInMemorySearchableRepository();
   });
 
-  describe('ApplyFilter method', () => {});
-  describe('ApplySort method', () => {});
-  describe('ApplyPaginate method', () => {});
-  describe('ApplySearch method', () => {});
+  describe('ApplyFilter method', () => {
+    it('Should no filter items when filter param is null', async () => {
+      const items = [new StubEntity({ name: 'name value', price: 50 })];
 
-  it('', async () => {});
+      const spyFilterMethod = jest.spyOn(items, 'filter');
+      const itemsFiltered = await sut['applyFilter'](items, null);
+      expect(itemsFiltered).toStrictEqual(items);
+      expect(spyFilterMethod).not.toHaveBeenCalled();
+    });
+
+    it('Should filter using the filter param', async () => {
+      const items = [
+        new StubEntity({ name: 'test', price: 50 }),
+        new StubEntity({ name: 'TEST', price: 50 }),
+        new StubEntity({ name: 'fake', price: 50 }),
+      ];
+
+      const spyFilterMethod = jest.spyOn(items, 'filter');
+      let itemsFiltered = await sut['applyFilter'](items, 'TEST');
+      expect(itemsFiltered).toStrictEqual([items[0], items[1]]);
+      expect(spyFilterMethod).toHaveBeenCalledTimes(1);
+
+      itemsFiltered = await sut['applyFilter'](items, 'test');
+      expect(itemsFiltered).toStrictEqual([items[0], items[1]]);
+      expect(spyFilterMethod).toHaveBeenCalledTimes(2);
+
+      itemsFiltered = await sut['applyFilter'](items, 'no-filter');
+      expect(itemsFiltered).toHaveLength(0);
+      expect(spyFilterMethod).toHaveBeenCalledTimes(3);
+    });
+  });
+
+  describe('ApplySort method', () => {});
+
+  describe('ApplyPaginate method', () => {});
+
+  describe('ApplySearch method', () => {});
 });
