ea942f5279bf6c9a74aa32b17faa80326d1a0026
GuilhermeMooser <guilhermemoserm@gmail.com>
2025-06-04 19:22:48 -0300

    Adding BCryptHashProvider unit tests

---
+++ src/users/infrastructure/providers/hash-provider/__tests__/unit/bcryptjs-hash.provider.spec.ts
@@ -0,0 +1,34 @@
+import { BCryptjsHashProvider } from '../../bcryptjs-hash.provider';
+
+describe('BCryptjsHashProvider unit tests', () => {
+  let sut: BCryptjsHashProvider;
+
+  beforeEach(() => {
+    sut = new BCryptjsHashProvider();
+  });
+
+  it('Should return encrypted password', async () => {
+    const password = 'TestPassword123';
+    const hash = await sut.generateHash(password);
+
+    expect(hash).toBeDefined();
+  });
+
+  it('Should return false on invalid password and hash comparison', async () => {
+    const password = 'TestPassword123';
+    const hash = await sut.generateHash(password);
+
+    const result = await sut.compareHash('fake', hash);
+
+    expect(result).toBeFalsy();
+  });
+
+  it('Should return true on valid password and hash comparison', async () => {
+    const password = 'TestPassword123';
+    const hash = await sut.generateHash(password);
+
+    const result = await sut.compareHash(password, hash);
+
+    expect(result).toBeTruthy();
+  });
+});
