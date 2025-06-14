e5d0dde18742eaec119924e284e03cbb4ad98a55
GuilhermeMooser <guilhermemoserm@gmail.com>
2025-06-14 16:50:41 -0300

    Ading DeleteUserUsecase

---
+++ src/users/application/usecases/delete-user.usecase.ts
@@ -0,0 +1,19 @@
+import { UserRepository } from '@/users/domain/repositories/user.repository';
+import { UseCase as DefaultUseCase } from '@/shared/application/usecases/use-case';
+
+/* eslint-disable @typescript-eslint/no-namespace */
+export namespace DeleteUserUseCase {
+  export type Input = {
+    id: string;
+  };
+
+  export type Output = void;
+
+  export class UseCase implements DefaultUseCase<Input, Output> {
+    constructor(private readonly userRepository: UserRepository.Repository) {}
+
+    async execute({ id }: Input): Promise<Output> {
+      await this.userRepository.delete(id);
+    }
+  }
+}
