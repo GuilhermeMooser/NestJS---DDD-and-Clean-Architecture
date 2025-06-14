7670182be35d08efc9da844d911386245c17f6c9
GuilhermeMooser <guilhermemoserm@gmail.com>
2025-06-14 17:05:43 -0300

    Adding SigIngUseCase

---
+++ src/users/application/usecases/sing-in.usecase.ts
@@ -0,0 +1,44 @@
+import { UserRepository } from '@/users/domain/repositories/user.repository';
+import { BadRequestError } from '../../../shared/application/errors/bad-request-error';
+import { HashProvider } from '@/shared/application/providers/hash-provider';
+import { UserOutput, UserOutputMapper } from '../dtos/user-output';
+import { UseCase as DefaultUseCase } from '@/shared/application/usecases/use-case';
+import { InvalidCrendialsError } from '@/shared/application/errors/invalid-credentials-error';
+
+/* eslint-disable @typescript-eslint/no-namespace */
+export namespace SigninUseCase {
+  export type Input = {
+    email: string;
+    password: string;
+  };
+
+  export type Output = UserOutput;
+
+  export class UseCase implements DefaultUseCase<Input, Output> {
+    constructor(
+      private readonly userRepository: UserRepository.Repository,
+      private readonly hashProvider: HashProvider,
+    ) {}
+
+    async execute(input: Input): Promise<Output> {
+      const { email, password } = input;
+
+      if (!email || !password) {
+        throw new BadRequestError('Input data not provided');
+      }
+
+      const entity = await this.userRepository.findByEmail(email);
+
+      const hashPasswordMatches = await this.hashProvider.compareHash(
+        password,
+        entity.password,
+      );
+
+      if (!hashPasswordMatches) {
+        throw new InvalidCrendialsError('Invalid credentials');
+      }
+
+      return UserOutputMapper.toOutput(entity);
+    }
+  }
+}
