50ad611d6b292bca2d60f2b63659b2131852e201
GuilhermeMooser <guilhermemoserm@gmail.com>
2025-06-15 20:43:23 -0300

    Registering SignupUseCase

--- src/users/infrastructure/users.module.ts
+++ src/users/infrastructure/users.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { SignupUseCase } from '../application/usecases/sign-up.usecase';
+import { UserInMemoryRepository } from './database/in-memory/repositories/user-in-memory.repository';
+import { BCryptjsHashProvider } from './providers/hash-provider/bcryptjs-hash.provider';
 
 @Module({
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [
+    UsersService,
+    {
+      provide: 'UserRepository',
+      useClass: UserInMemoryRepository,
+    },
+    {
+      provide: 'HashProvider',
+      useClass: BCryptjsHashProvider,
+    },
+    {
+      provide: SignupUseCase.UseCase,
+      useClass: SignupUseCase.UseCase,
+    },
+  ],
 })
 export class UsersModule {}
