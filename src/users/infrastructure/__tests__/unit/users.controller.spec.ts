0d3830b1b8f8f1bae1dc57fa4a7918af4ce681c2
GuilhermeMooser <guilhermemoserm@gmail.com>
2025-06-18 15:15:39 -0300

    UsersController unit tests - search method

--- src/users/infrastructure/__tests__/unit/users.controller.spec.ts
+++ src/users/infrastructure/__tests__/unit/users.controller.spec.ts
@@ -10,6 +10,7 @@ import { UpdateUserDto } from '../../dtos/update-user.dto';
 import { UpdatePasswordUseCase } from '@/users/application/usecases/update-password.usecase';
 import { UpdatePasswordDto } from '../../dtos/update-password.dto';
 import { GetUserUseCase } from '@/users/application/usecases/get-user.usecase ';
+import { ListUsersUseCase } from '@/users/application/usecases/list-users.usecase';
 
 describe('UsersController unit tests', () => {
   let sut: UsersController;
@@ -141,5 +142,31 @@ describe('UsersController unit tests', () => {
     expect(mockGetUserUseCase.execute).toHaveBeenCalledWith({
       id,
     });
+  });
+
+  it('should list users', async () => {
+    const output: ListUsersUseCase.Output = {
+      items: [props],
+      currentPage: 1,
+      lastPage: 1,
+      perPage: 1,
+      total: 1,
+    };
+
+    const mockListUsersUseCase = {
+      execute: jest.fn().mockReturnValue(Promise.resolve(output)),
+    };
+
+    sut['listUsersUseCase'] = mockListUsersUseCase as any;
+
+    const searchParams = {
+      page: 1,
+      perPage: 1,
+    };
+
+    const result = await sut.search(searchParams);
+
+    expect(output).toStrictEqual(result);
+    expect(mockListUsersUseCase.execute).toHaveBeenCalledWith(searchParams);
   });
 });
