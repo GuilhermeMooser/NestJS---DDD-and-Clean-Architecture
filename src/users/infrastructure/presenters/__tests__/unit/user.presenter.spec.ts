753f8db496bd2070a668b7f4c65ecd479367b4c4
GuilhermeMooser <guilhermemoserm@gmail.com>
2025-07-21 20:10:43 -0300

    Adding UserPresenter class pt 2

--- src/users/infrastructure/presenters/__tests__/unit/user.presenter.spec.ts
+++ src/users/infrastructure/presenters/__tests__/unit/user.presenter.spec.ts
@@ -1,23 +1,40 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
 import { UserPresenter } from '../../user.presenter';
+import { instanceToPlain } from 'class-transformer';
 
 describe('UsersPresenter unit tests', () => {
   const createdAt = new Date();
 
-  let props = {
+  const props = {
     id: '12312a',
     name: 'test name',
     email: 'a@a.com',
     password: 'fake',
     createdAt: createdAt,
   };
+  let sut: UserPresenter;
+  beforeEach(() => {
+    sut = new UserPresenter(props);
+  });
 
   describe('constructor', () => {
     it('Should be defined', () => {
-      const sut = new UserPresenter(props);
       expect(sut.id).toEqual(props.id);
       expect(sut.name).toEqual(props.name);
       expect(sut.email).toEqual(props.email);
       expect(sut.createdAt).toEqual(props.createdAt);
+    });
+  });
+
+  it('Should presenter data', () => {
+    const output = instanceToPlain(sut);
+    expect(output).toStrictEqual({
+      id: '12312a',
+      name: 'test name',
+      email: 'a@a.com',
+      createdAt: createdAt.toISOString(),
     });
   });
 });
