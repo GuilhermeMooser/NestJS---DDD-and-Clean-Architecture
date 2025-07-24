a5e851c8951e783e4161f40e0ca9bdf3c7b69806
GuilhermeMooser <guilhermemoserm@gmail.com>
2025-07-23 21:52:08 -0300

    Config Validation Pipe and DTOs

--- src/users/infrastructure/dtos/sign-in.dto.ts
+++ src/users/infrastructure/dtos/sign-in.dto.ts
@@ -1,6 +1,13 @@
 import { SigninUseCase } from '@/users/application/usecases/sign-in.usecase';
+import { IsString, IsNotEmpty, IsEmail } from 'class-validator';
 
 export class SigninDto implements SigninUseCase.Input {
+  @IsString()
+  @IsNotEmpty()
+  @IsEmail()
   email: string;
+
+  @IsString()
+  @IsNotEmpty()
   password: string;
 }
