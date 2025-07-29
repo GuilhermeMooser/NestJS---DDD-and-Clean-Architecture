3d2d8aecba9fdc97dc2a22dd2090ff7a2366a4b5
GuilhermeMooser <guilhermemoserm@gmail.com>
2025-07-28 21:20:19 -0300

    UserController e2e test - update method - pt 3

---
+++ src/users/infrastructure/__tests__/e2e/update.e2e-specy.ts
@@ -0,0 +1,150 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+import { applyGlobalConfig } from '@/global-config';
+import { DatabaseModule } from '@/shared/infrastructure/database/database.module';
+import { setupPrismaTests } from '@/shared/infrastructure/database/prisma/testing/setup-prisma-tests';
+import { EnvConfigModule } from '@/shared/infrastructure/env-config/env-config.module';
+import { UserEntity } from '@/users/domain/entities/user.entity';
+import { UserRepository } from '@/users/domain/repositories/user.repository';
+import { UserDataBuilder } from '@/users/domain/testing/helping/user-data-builder';
+import { UsersController } from '@/users/infrastructure/users.controller';
+import { UsersModule } from '@/users/infrastructure/users.module';
+import { INestApplication } from '@nestjs/common';
+import { Test, TestingModule } from '@nestjs/testing';
+import { PrismaClient } from '@prisma/client';
+import { instanceToPlain } from 'class-transformer';
+import request from 'supertest';
+import { UpdateUserDto } from '../../dtos/update-user.dto';
+
+describe('UsersController unit tests', () => {
+  let app: INestApplication;
+  let module: TestingModule;
+  let repository: UserRepository.Repository;
+  let updateUserDto: UpdateUserDto;
+  const prismaService = new PrismaClient();
+  let entity: UserEntity;
+
+  beforeAll(async () => {
+    setupPrismaTests();
+    module = await Test.createTestingModule({
+      imports: [
+        EnvConfigModule,
+        UsersModule,
+        DatabaseModule.forTest(prismaService),
+      ],
+    }).compile();
+
+    app = module.createNestApplication();
+    applyGlobalConfig(app);
+    await app.init();
+    repository = module.get<UserRepository.Repository>('UserRepository');
+  });
+
+  beforeEach(async () => {
+    updateUserDto = {
+      name: 'test name',
+    };
+    await prismaService.user.deleteMany();
+    entity = new UserEntity(UserDataBuilder({}));
+    await repository.insert(entity);
+  });
+
+  describe('PUT /users/:id', () => {
+    it('should update an user', async () => {
+      updateUserDto.name = 'test name';
+      const res = await request(app.getHttpServer())
+        .put(`/users/${entity._id}`)
+        .send(updateUserDto)
+        .expect(200);
+
+      const user = await repository.findById(entity._id);
+      const presenter = UsersController.userToResponse(user.toJSON());
+      const serialized = instanceToPlain(presenter);
+      expect(res.body.data).toStrictEqual(serialized);
+    });
+
+    it('should return a error with 422 code when request body is invalid', async () => {
+      const res = await request(app.getHttpServer())
+        .put(`/users/${entity._id}`)
+        .send({})
+        .expect(422);
+
+      expect(res.body.error).toBe('Unprocessable Entity');
+      expect(res.body.message).toEqual([
+        'name should not be empty',
+        'name must be a string',
+      ]);
+    });
+
+    // it('should return a error with 422 code when request name field is invalid', async () => {
+    //   delete signUpDto.name;
+    //   const res = await request(app.getHttpServer())
+    //     .post('/users')
+    //     .send({ signUpDto })
+    //     .expect(422);
+
+    //   expect(res.body.error).toBe('Unprocessable Entity');
+    //   expect(res.body.message).toEqual([
+    //     'name should not be empty',
+    //     'name must be a string',
+    //   ]);
+    // });
+
+    // it('should return a error with 422 code when request email field is invalid', async () => {
+    //   delete signUpDto.email;
+    //   const res = await request(app.getHttpServer())
+    //     .post('/users')
+    //     .send({ signUpDto })
+    //     .expect(422);
+
+    //   expect(res.body.error).toBe('Unprocessable Entity');
+    //   expect(res.body.message).toEqual([
+    //     'email must be an email',
+    //     'email should not be empty',
+    //     'email must be a string',
+    //   ]);
+    // });
+
+    // it('should return a error with 422 code when request name password is invalid', async () => {
+    //   delete signUpDto.password;
+    //   const res = await request(app.getHttpServer())
+    //     .post('/users')
+    //     .send({ signUpDto })
+    //     .expect(422);
+
+    //   expect(res.body.error).toBe('Unprocessable Entity');
+    //   expect(res.body.message).toEqual([
+    //     'password should not be empty',
+    //     'password must be a string',
+    //   ]);
+    // });
+
+    // it('should return a error with 422 code with invalid field provided', async () => {
+    //   delete signUpDto.password;
+    //   const res = await request(app.getHttpServer())
+    //     .post('/users')
+    //     .send(Object.assign(signUpDto, { xpto: 'fake' }))
+    //     .expect(422);
+
+    //   expect(res.body.error).toBe('Unprocessable Entity');
+    //   expect(res.body.message).toEqual(['property xpt should not exists']);
+    // });
+
+    // it('should return a error with 409 code when the email is duplicated', async () => {
+    //   const entity = new UserEntity(UserDataBuilder({ ...signUpDto }));
+    //   await repository.insert(entity);
+
+    //   const res = await request(app.getHttpServer())
+    //     .post('/users')
+    //     .send(signUpDto)
+    //     .expect(409)
+    //     .expect({
+    //       statusCode: 409,
+    //       error: 'Conflict',
+    //       message: 'Email address already used',
+    //     });
+    // });
+  });
+});
