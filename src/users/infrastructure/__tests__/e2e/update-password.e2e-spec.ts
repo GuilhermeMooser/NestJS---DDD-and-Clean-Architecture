204c4b28494ae711b31628d4238d2637b14284ea
GuilhermeMooser <guilhermemoserm@gmail.com>
2025-07-30 22:28:27 -0300

    Refactoring e2e tests of updatePassword method

--- src/users/infrastructure/__tests__/e2e/update-password.e2e-spec.ts
+++ src/users/infrastructure/__tests__/e2e/update-password.e2e-spec.ts
@@ -26,6 +26,7 @@ describe('UsersController e2e tests', () => {
   const prismaService = new PrismaClient();
   let hashProvider: HashProvider;
   let entity: UserEntity;
+  let accessToken: string;
 
   beforeAll(async () => {
     setupPrismaTests();
@@ -51,14 +52,25 @@ describe('UsersController e2e tests', () => {
     };
     await prismaService.user.deleteMany();
     const hashPassword = await hashProvider.generateHash('old_password');
-    entity = new UserEntity(UserDataBuilder({ password: hashPassword }));
+    entity = new UserEntity(
+      UserDataBuilder({ email: 'a@a.com', password: hashPassword }),
+    );
     await repository.insert(entity);
+    const loginResponse = await request(app.getHttpServer())
+      .post(`/users/login`)
+      .send({
+        email: 'a@a.com',
+        password: 'old_password',
+      })
+      .expect(200);
+    accessToken = loginResponse.body.accessToken;
   });
 
   describe('PATCH /users', () => {
     it('should update a password', async () => {
       const res = await request(app.getHttpServer())
         .patch(`/users/${entity._id}`)
+        .set('Authorization', `Bearer ${accessToken}`)
         .send(updatePasswordDto)
         .expect(200);
 
@@ -75,6 +87,7 @@ describe('UsersController e2e tests', () => {
     it('should return a error with 422 code when request body is invalid', async () => {
       const res = await request(app.getHttpServer())
         .patch('/users/fakeId')
+        .set('Authorization', `Bearer ${accessToken}`)
         .send({})
         .expect(422);
 
@@ -90,6 +103,7 @@ describe('UsersController e2e tests', () => {
     it('should return a error with 404 code when throw NotFoundError with invalid id', async () => {
       const res = await request(app.getHttpServer())
         .patch('/users/fakeId')
+        .set('Authorization', `Bearer ${accessToken}`)
         .send(updatePasswordDto)
         .expect(404);
 
@@ -101,6 +115,7 @@ describe('UsersController e2e tests', () => {
       delete updatePasswordDto.password;
       const res = await request(app.getHttpServer())
         .patch(`/users/${entity._id}`)
+        .set('Authorization', `Bearer ${accessToken}`)
         .send(updatePasswordDto)
         .expect(422);
 
@@ -115,6 +130,7 @@ describe('UsersController e2e tests', () => {
       delete updatePasswordDto.oldPassword;
       const res = await request(app.getHttpServer())
         .patch(`/users/${entity._id}`)
+        .set('Authorization', `Bearer ${accessToken}`)
         .send(updatePasswordDto)
         .expect(422);
 
@@ -129,6 +145,7 @@ describe('UsersController e2e tests', () => {
       updatePasswordDto.oldPassword = 'fake';
       const res = await request(app.getHttpServer())
         .patch(`/users/${entity._id}`)
+        .set('Authorization', `Bearer ${accessToken}`)
         .send(updatePasswordDto)
         .expect(422)
         .expect({
@@ -138,5 +155,15 @@ describe('UsersController e2e tests', () => {
         });
     });
 
+    it('should return a error with 401 when the request is not authorized', async () => {
+      const res = await request(app.getHttpServer())
+        .patch('/users/fakeId')
+        .set('Authorization', `Bearer ${accessToken}`)
+        .expect(401)
+        .expect({
+          statusCode: 401,
+          message: 'Unauthorized',
+        });
+    });
   });
 });
