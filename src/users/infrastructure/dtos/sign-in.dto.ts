46e14b7b9a0cccdd90d695558dfa69ec77b383b5
GuilhermeMooser <guilhermemoserm@gmail.com>
2025-08-01 23:24:10 -0300

    Refactoring the DTOs with the documentation

--- src/users/infrastructure/dtos/sign-in.dto.ts
+++ src/users/infrastructure/dtos/sign-in.dto.ts
@@ -1,12 +1,15 @@
 import { SigninUseCase } from '@/users/application/usecases/sign-in.usecase';
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString, IsNotEmpty, IsEmail } from 'class-validator';
 
 export class SigninDto implements SigninUseCase.Input {
+  @ApiProperty({ description: 'Email do usuário' })
   @IsString()
   @IsNotEmpty()
   @IsEmail()
   email: string;
 
+  @ApiProperty({ description: 'Senha do usuário' })
   @IsString()
   @IsNotEmpty()
   password: string;
